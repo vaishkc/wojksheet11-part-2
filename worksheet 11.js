bc36e3ecf478fb911f51bdb1c90fc2cde90b364b
vaishkc <115457325+vaishkc@users.noreply.github.com>
2022-11-17 21:06:20 +0530

    Add files via upload

---
+++ worksheet 11.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colors!</title>
+    <style>
+        h1{
+            font-family:Courier New;
+            text-align:center;
+            background-color:#6be7ad
+        }
+        .h{
+            font-family:Tahoma;
+            text-align:center;
+            font-style: bold;
+            font-size: 15px;
+}
+        input[type=text]:focus {
+            width: 100%;
+        }
+        button {
+            border: none;
+            color: black;
+            padding: 16px 32px;
+            text-decoration: none;
+            margin: 4px 2px;
+            cursor: pointer;
+            font-size: 20px;
+            width: 100%;
+        }
+    </style>
+</head>
+<body bgcolor="FFFDD1">
+    <h1>Color Buttons</h1>
+    <button id="btn" style="background-color: salmon">Click me</button>
+    <script src="index.js">
+</script>
+</body>
+</html>
+
